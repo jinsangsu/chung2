378641606094f0605a778596197c8deddb59dd8e
jinsangsu <licjssj777@gmail.com>
2025-07-18 10:20:51 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ with st.form("input_form", clear_on_submit=True):
         )
     with col2:
         # ★ 버튼 위에 여백 추가!
-        st.markdown("<div style='height:5px;'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='height:0px;'></div>", unsafe_allow_html=True)
         submitted = st.form_submit_button("질문하기", use_container_width=True)
 
     if submitted and question_input:
